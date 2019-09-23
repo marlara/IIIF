c7c1a5534a2628c0a95059004e761abf141fe3ba
marlara <marlara@users.noreply.github.com>
2019-09-23 12:31:28 +0200

    Update harvest.py
    script changed

--- harvest.py
+++ harvest.py
@@ -1,54 +1,31 @@
+import re
+import requests_cache
+import json
+import requests
 import os
 import tldextract
-import requests_cache
-import requests
-import hashlib
-import json
+import argparse
 
 requests_cache.install_cache('cache')
 
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/76.0.3809.100 Chrome/76.0.3809.100 Safari/537.36'
+headers = {'User-Agent': user_agent}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', type = str)
+args = parser.parse_args()
+
+ext = tldextract.extract(args.url)
+
+os.chdir(ext.domain)
 
 file = open("list-manifest", "r")
-
-def save_manifest():
-	user_agent = '' #your user agent
-	headers = {'User-Agent': user_agent}
-	h = hashlib.sha256()
-	global file
-	
-	next(file) #go to the next line
-	for line in file:
-		if "Collection:" in line:
-			os.chdir("..")
-			collection_url = line.strip("Collection: ").strip("\n")
-			cl_ext = collection_url.split("/")[-1] #create a subfolder with the name of the collection (this logic is not perfect)
-			print(cl_ext)
-			os.mkdir(cl_ext)
-			os.chdir(cl_ext)
-			break
-		else:
-			r = requests.get(line.strip("\n"), headers=headers, verify=False).json()
-			f = open(h.hexdigest()+".json", "w")
-			json.dump(r, f)
-			print(line)
-
-
 for line in file:
-	if "Url:" in line:
-		url = line.strip("Url: ")
-		print(url)
-		ext = tldextract.extract(url)
-		print(ext.domain)
-		os.mkdir(ext.domain) #create a directory named after the general url domain
-		os.chdir(ext.domain) #change into that directory
-	elif "Collection:" in line:
-		collection_url = line.strip("Collection: ").strip("\n") #create a subfolder with the name of the collection
-		cl_ext = collection_url.split("/")[-1]
-		print(cl_ext)
-		os.mkdir(cl_ext)
-		os.chdir(cl_ext)
-		save_manifest()
-	else:
-		save_manifest()
+	r = requests.get(line.strip("\n"), headers=headers, verify=False).json()
+	name_search = re.search(r'([^\/]+)\/manifest$', line)
+	name = name_search.group(1)+".json"
+	print(name)
+	f = open(name+".json", "w")
+	json.dump(r, f)
+	print(line)
 print("Done")
-file.close()
